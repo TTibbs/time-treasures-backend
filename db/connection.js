19e25fd76819b9057cc8acaa3109e8b0a5b17aaf
Benedict Robinson <benedict.r1713@gmail.com>
2024-11-25 12:17:00 +0000

    Requirements to host

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,17 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PG Database not set");
 }
 
-// && !process.env.DATABASE_URL
 
-// const config = {};
 
-// if (ENV === "production") {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.max = 2;
-// }
+const config = {};
 
-module.exports = new Pool();
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
